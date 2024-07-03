37b1bdc91b7f4a284cb1e7c7ad5e1dc83a2f60b6
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-07-03 22:46:20 +0300

    Update TestRecursion.java

--- Recursion and Backtracking/src/TestRecursion.java
+++ Recursion and Backtracking/src/TestRecursion.java
@@ -1,22 +1,32 @@
 public class TestRecursion {
     public static void main(String[] args) {
+        System.out.print("(main");
         a();
+        System.out.print("main)");
+        System.out.println();
     }
 
     private static void a() {
+        System.out.print("(a");
         b();
+        System.out.print("a)");
     }
 
     private static void b() {
+        System.out.print("(b");
         c();
+        System.out.print("b)");
     }
 
     private static void c() {
+        System.out.print("(c");
         d();
+        System.out.print("c)");
     }
 
     private static void d() {
-
+        System.out.print("(d)");
     }
+
 
 }
