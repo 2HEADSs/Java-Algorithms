1330c61360f1cd50c03762f6bf49619ec38923a7
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-07-07 22:44:05 +0300

    Create RecursiveDrawing.java

---
+++ Recursion and Backtracking/src/RecursiveDrawing.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursiveDrawing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int f = n;
+
+        drawFigure(n, f);
+    }
+
+    private static void drawFigure(int n, int f) {
+        if (n <= 0) {
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        drawFigure(n - 1, f + 1);
+    }
+
+}
