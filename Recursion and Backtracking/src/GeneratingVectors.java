423483952d2c6d0460b9ba2371ea9f5fa055cdf8
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-07-10 14:27:48 +0300

    Create GeneratingVectors.java

---
+++ Recursion and Backtracking/src/GeneratingVectors.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GeneratingVectors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Integer[] memory = new Integer[n];
+
+        fillVector(memory, memory.length - 1);
+    }
+
+    private static void fillVector(Integer[] memory, int index) {
+        if (index < 0) {
+            print(memory);
+        }
+        fillVector(memory, index - 1);
+    }
+
+    private static void print(Integer[] memory) {
+        for (Integer integer : memory) {
+            System.out.println(integer);
+        }
+        System.out.println();
+    }
+}
