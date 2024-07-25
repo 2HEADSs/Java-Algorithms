288db4fa6d530ce95a7fb9e255e84f2a2d94f87c
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-07-25 20:59:05 +0300

    Create BinarySearch.java

---
+++ SearchingSortingAndGreedyAlgorithms/src/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[]arr = {13,2,34,73,24,86};
+        System.out.println(indexOf(arr,73));
+    }
+
+    private static int indexOf(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int current = arr[mid];
+            if (key < current) {
+                end = mid - 1;
+            } else if (key > current) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
